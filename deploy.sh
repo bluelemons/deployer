4b79499cadb6dafec31379dd6426815de3c80166
Eloy Espinaco <eloyesp@gmail.com>
2016-03-07 15:38:17 -0300

    Authenticate request for heroku

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,18 @@ app=muebleria
 remote_env=$1
 remote_url=$2
 tag=$3
+token=$4
 
 if [ -d $app ]
 then (cd $app && git pull)
 else git clone https://github.com/bluelemons/muebleria.git
 fi
 
+cat <<NETRC > ~/.netrc
+machine api.heroku.com
+  login auser
+  password $token
+NETRC
+
 cd $app
-git add remote $remote_env $remote_url
-git push --force $tag:master
+git push --force $remote_url $tag:master
